e0b09e2c243c24fe034c13ed42376907fc7db576
fert.arthur <fert.arthur83@gmail.com>
2025-02-10 22:21:23 +0100

    minor changes

--- Detection.py
+++ Detection.py
@@ -3,12 +3,12 @@ import win32api
 import win32file
 os.system("cls")
 drive_types = {
-                win32file.DRIVE_UNKNOWN : "Unknown\nDrive type can't be determined.",
-                win32file.DRIVE_REMOVABLE : "Removable\nDrive has removable media. This includes all floppy drives and many other varieties of storage devices.",
-                win32file.DRIVE_FIXED : "Fixed\nDrive has fixed (nonremovable) media. This includes all hard drives, including hard drives that are removable.",
-                win32file.DRIVE_REMOTE : "Remote\nNetwork drives. This includes drives shared anywhere on a network.",
-                win32file.DRIVE_CDROM : "CDROM\nDrive is a CD-ROM. No distinction is made between read-only and read/write CD-ROM drives.",
-                win32file.DRIVE_RAMDISK : "RAMDisk\nDrive is a block of random access memory (RAM) on the local computer that behaves like a disk drive.",
+                win32file.DRIVE_UNKNOWN : "Unknown\n   Drive type can't be determined.",
+                win32file.DRIVE_REMOVABLE : "Removable\n   Drive has removable media. This includes all floppy drives and many other varieties of storage devices.",
+                win32file.DRIVE_FIXED : "Fixed\n   Drive has fixed (nonremovable) media. This includes all hard drives, including hard drives that are removable.",
+                win32file.DRIVE_REMOTE : "Remote\n   Network drives. This includes drives shared anywhere on a network.",
+                win32file.DRIVE_CDROM : "CDROM\n   Drive is a CD-ROM. No distinction is made between read-only and read/write CD-ROM drives.",
+                win32file.DRIVE_RAMDISK : "RAMDisk\n   Drive is a block of random access memory (RAM) on the local computer that behaves like a disk drive.",
                 win32file.DRIVE_NO_ROOT_DIR : "The root directory does not exist."
               }
 
@@ -16,8 +16,9 @@ drives = win32api.GetLogicalDriveStrings().split('\x00')[:-1]
 
 for device in drives:
     type = win32file.GetDriveType(device)
-    
+    info = win32api.GetVolumeInformation(device)
     print("Drive: %s" % device)
+    print("Volume: %s" % info[0])
     print(drive_types[type])
     print("-"*72)
 
